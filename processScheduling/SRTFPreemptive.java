2570974c1f2a234f09cc80ed77143ac9b587c4e3
Aarav Lodha <lodhaaarav12005@gmail.com>
2024-05-12 17:29:09 +0530

    added srtf code

--- processScheduling/SRTFPreemptive.java
+++ processScheduling/SRTFPreemptive.java
@@ -8,13 +8,15 @@ public class SRTFPreemptive
         Scanner scanner = new Scanner(System.in);
         System.out.print("\nEnter the number of Processes: ");
         int n = scanner.nextInt();
+        int used[] = new int[n]; 
+        int time=0,y=n;
 
         int[] at = new int[n]; 
         int[] bt = new int[n]; 
         int[] x = new int[n]; 
         int[] waiting = new int[n];
         int[] turnaround = new int[n];
-        int[] completion = new int[n];
+        int[] ct = new int[n];
 
         for (int i = 0; i < n; i++) 
         {
@@ -29,44 +31,49 @@ public class SRTFPreemptive
             x[i] = bt[i]; 
         }
 
-        int count = 0, time = 0, smallest;
-        double avg = 0, tt = 0, end;
-        int index = -1;
+        int minBurst=999;
+        int index=-1;
 
-        for (time = 0; count != n; time++) 
+        while(y!=0)
         {
-            smallest = Integer.MAX_VALUE; 
-            for (int i = 0; i < n; i++) 
+            for(int i=0;i<n;i++)
             {
-                if (at[i] <= time && bt[i] < smallest && bt[i] > 0) 
+                if(at[i]<=time && bt[i]<minBurst && used[i]==0)
                 {
-                    smallest = bt[i];
+                    minBurst=bt[i];
                     index=i;
                 }
             }
-
-            bt[index]--;
-            if (bt[index] == 0) 
+            if(index==-1)
             {
-                count++;
-                end = time + 1;
-                completion[index] = (int) end;
-                turnaround[index] = (int) (end - at[index]);
-                waiting[index] = (int) (turnaround[index] - x[index]);
+                time++;
             }
+            else
+            {
+                time++;
+                bt[index]--;
+                if(bt[index]==0)
+                {
+                    used[index]=1;
+                    ct[index]=time;
+                    y--;
+                }
+                index=-1;
+                minBurst=999;
+            }
+        }
 
+        for (int i = 0; i < n; i++) 
+        {
+            turnaround[i] = ct[i] - at[i];
+            waiting[i] = turnaround[i] - x[i];
         }
 
         System.out.println("Process\tBurst-Time\tArrival-Time\tWaiting-Time\tTurnaround-Time\tCompletion-Time");
         for (int i = 0; i < n; i++) 
         {
-            System.out.println("p" + (i + 1) + "\t\t" + x[i] + "\t\t" + at[i] + "\t\t" + waiting[i] + "\t\t" + turnaround[i] + "\t\t" + completion[i]);
-            avg += waiting[i];
-            tt += turnaround[i];
-        }
-
-        System.out.println("\n\nAverage waiting time =" + (avg / n));
-        System.out.println("Average Turnaround time =" + (tt / n));
+            System.out.println("p" + (i + 1) + "\t\t" + x[i] + "\t\t" + at[i] + "\t\t" + waiting[i] + "\t\t" + turnaround[i] + "\t\t" + ct[i]);
+        } 
         scanner.close();
     }
 }
